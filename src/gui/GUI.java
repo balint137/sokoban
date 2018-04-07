8f6cc3e532a103ee7f6957b65a8ea5c9d168f3c7
balint137 <dobszi14@gmail.com>
2018-04-07 14:13:27 +0200

    10x10 mezős pálya

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -27,13 +27,21 @@ import common.IGameState;
 public class GUI extends JFrame implements IGameState {
 	private static final long serialVersionUID = 7803853539866953138L;
 	
+	private JTable table;
+	private JPanel tablePanel;
+	private Icon player;
+	private Icon wall;
+	private Icon crate;
+	private Icon target;
+	private Icon ground;
+	
 	GUI() {
 		super("Sokoban");
-		setSize(700, 700);
+		setSize(1000, 800);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setResizable(false);
 		setLayout(null);
 		
-		//create menu
 		JMenuBar menuBar = new JMenuBar();
 
 		JMenu menu = new JMenu("Start");
@@ -66,7 +74,6 @@ public class GUI extends JFrame implements IGameState {
 
 		setJMenuBar(menuBar);
 		
-		//load pictures
 		BufferedImage groundImage = null;
 		try {
 			groundImage = ImageIO.read(new File("resources/ground.png"));
@@ -106,60 +113,46 @@ public class GUI extends JFrame implements IGameState {
 		Graphics graphics = combined.getGraphics();
 		graphics.drawImage(groundImage, 0, 0, null);
 		graphics.drawImage(playerImage, 0, 0, null);
-		Icon player = new ImageIcon(combined);
+		player = new ImageIcon(combined);
 		
 		combined = new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB);
 		graphics = combined.getGraphics();
 		graphics.drawImage(groundImage, 0, 0, null);
 		graphics.drawImage(wallImage, 0, 0, null);
-		Icon wall = new ImageIcon(combined);
+		wall = new ImageIcon(combined);
 		
 		combined = new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB);
 		graphics = combined.getGraphics();
 		graphics.drawImage(groundImage, 0, 0, null);
 		graphics.drawImage(crateImage, 0, 0, null);
-		Icon crate = new ImageIcon(combined);
+		crate = new ImageIcon(combined);
 		
 		combined = new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB);
 		graphics = combined.getGraphics();
 		graphics.drawImage(groundImage, 0, 0, null);
 		graphics.drawImage(targetImage, 0, 0, null);
-		Icon target = new ImageIcon(combined);
+		target = new ImageIcon(combined);
 		
-		Icon ground = new ImageIcon(groundImage);
+		ground = new ImageIcon(groundImage);
+		
+		table = new JTable(10, 10) {
+			private static final long serialVersionUID = -4882552328598802471L;
 
-		
-        String[] columnNames = {"1", "2", "3", "4", "5"};
-        Object[][] data = {
-            {wall, wall, wall, wall, wall},
-            {wall, ground, wall, crate, wall},
-            {wall, ground, ground, ground, wall},
-            {wall, target, crate, player, wall},
-            {wall, wall, wall, wall, wall},
-        };
-		
-		JTable table = new JTable(data, columnNames) {
-			private static final long serialVersionUID = 1L;
-			
 			@Override
 			public Class<?> getColumnClass(int column) {
                 return getValueAt(0, column).getClass();
             }
 		};
-		
-		TableColumnModel columnModel = table.getColumnModel();
-		columnModel.getColumn(0).setPreferredWidth(64);
-		columnModel.getColumn(1).setPreferredWidth(64);		
-		columnModel.getColumn(2).setPreferredWidth(64);
-		columnModel.getColumn(3).setPreferredWidth(64);
-		columnModel.getColumn(4).setPreferredWidth(64);
-		table.setRowHeight(64);
+		table.setShowGrid(false);
 		table.setIntercellSpacing(new Dimension(0, 0));
+		table.setRowHeight(64);
+		for (int i = 0; i < table.getColumnCount(); i++) {
+			table.getColumnModel().getColumn(i).setPreferredWidth(64);
+		}
 		
-		JPanel tablePanel = new JPanel();
+		tablePanel = new JPanel();
 		tablePanel.add(table);
-		tablePanel.setBounds(100, 100, 350, 350);
-		tablePanel.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+		tablePanel.setBounds(50, 50, 700, 700);
 		add(tablePanel);
 		
 		setVisible(true);
