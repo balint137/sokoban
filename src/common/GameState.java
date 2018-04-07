8a116d57163cbd6d604bf3f6ee7abdeb64dd5ea2
balint137 <dobszi14@gmail.com>
2018-04-07 14:08:06 +0200

    Pálya mező típusok

--- src/common/GameState.java
+++ src/common/GameState.java
@@ -4,4 +4,7 @@ import java.io.Serializable;
 
 public class GameState implements Serializable {
 	private static final long serialVersionUID = 7413938669227547696L;
+
+	public enum FieldType { GROUND, WALL, PLAYER1, PLAYER2, CRATE, TARGET }
+	public FieldType[][] fields;
 }
