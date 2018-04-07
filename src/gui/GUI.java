d90ba6c1d6f706877fee57fe7f95b87ae15c900d
balint137 <dobszi14@gmail.com>
2018-04-07 10:02:13 +0200

    Új mező ikonok

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -2,7 +2,9 @@ package gui;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Image;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -28,50 +30,136 @@ public class GUI extends JFrame implements IGameState {
 	
 	GUI() {
 		super("Sokoban");
-		setSize(600, 600);
+		setSize(700, 700);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLayout(null);
 		
+		//create menu
 		JMenuBar menuBar = new JMenuBar();
+
 		JMenu menu = new JMenu("Start");
+
 		JMenuItem menuItem = new JMenuItem("Client");
+		menuItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		menu.add(menuItem);
+
+		menuItem = new JMenuItem("Server");
+		menuItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		menu.add(menuItem);
 		menuBar.add(menu);
+		
+		menuItem = new JMenuItem("Exit");
+		menuItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		menuBar.add(menuItem);
+
 		setJMenuBar(menuBar);
 		
-		BufferedImage myPicture = null;
+		//load pictures
+		BufferedImage groundImage = null;
 		try {
-			myPicture = ImageIO.read(new File("icon.png"));
+			groundImage = ImageIO.read(new File("resources/ground.png"));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		Icon icon = new ImageIcon(myPicture.getScaledInstance(80, 80, Image.SCALE_FAST));
+		BufferedImage playerImage = null;
+		try {
+			playerImage = ImageIO.read(new File("resources/player.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
-        String[] columnNames = {"1", "2", "3"};
+		BufferedImage wallImage = null;
+		try {
+			wallImage = ImageIO.read(new File("resources/wall.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		BufferedImage crateImage = null;
+		try {
+			crateImage = ImageIO.read(new File("resources/crate.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		BufferedImage targetImage = null;
+		try {
+			targetImage = ImageIO.read(new File("resources/target.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		BufferedImage combined = new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB);
+		Graphics graphics = combined.getGraphics();
+		graphics.drawImage(groundImage, 0, 0, null);
+		graphics.drawImage(playerImage, 0, 0, null);
+		Icon player = new ImageIcon(combined);
+		
+		combined = new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB);
+		graphics = combined.getGraphics();
+		graphics.drawImage(groundImage, 0, 0, null);
+		graphics.drawImage(wallImage, 0, 0, null);
+		Icon wall = new ImageIcon(combined);
+		
+		combined = new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB);
+		graphics = combined.getGraphics();
+		graphics.drawImage(groundImage, 0, 0, null);
+		graphics.drawImage(crateImage, 0, 0, null);
+		Icon crate = new ImageIcon(combined);
+		
+		combined = new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB);
+		graphics = combined.getGraphics();
+		graphics.drawImage(groundImage, 0, 0, null);
+		graphics.drawImage(targetImage, 0, 0, null);
+		Icon target = new ImageIcon(combined);
+		
+		Icon ground = new ImageIcon(groundImage);
+
+		
+        String[] columnNames = {"1", "2", "3", "4", "5"};
         Object[][] data = {
-            {icon, icon, icon},
-            {icon, icon, icon},
-            {icon, icon, icon},
+            {wall, wall, wall, wall, wall},
+            {wall, ground, wall, crate, wall},
+            {wall, ground, ground, ground, wall},
+            {wall, target, crate, player, wall},
+            {wall, wall, wall, wall, wall},
         };
 		
 		JTable table = new JTable(data, columnNames) {
-			public Class getColumnClass(int column) {
+			private static final long serialVersionUID = 1L;
+			
+			@Override
+			public Class<?> getColumnClass(int column) {
                 return getValueAt(0, column).getClass();
             }
 		};
 		
 		TableColumnModel columnModel = table.getColumnModel();
-		columnModel.getColumn(0).setPreferredWidth(80);
-		columnModel.getColumn(1).setPreferredWidth(80);		
-		columnModel.getColumn(2).setPreferredWidth(80);
-		table.setRowHeight(80);
+		columnModel.getColumn(0).setPreferredWidth(64);
+		columnModel.getColumn(1).setPreferredWidth(64);		
+		columnModel.getColumn(2).setPreferredWidth(64);
+		columnModel.getColumn(3).setPreferredWidth(64);
+		columnModel.getColumn(4).setPreferredWidth(64);
+		table.setRowHeight(64);
 		table.setIntercellSpacing(new Dimension(0, 0));
 		
 		JPanel tablePanel = new JPanel();
 		tablePanel.add(table);
-		tablePanel.setBounds(100, 100, 300, 300);
+		tablePanel.setBounds(100, 100, 350, 350);
 		tablePanel.setBorder(BorderFactory.createLineBorder(Color.BLUE));
 		add(tablePanel);
 		
