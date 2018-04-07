9543c1edfa9d9e8ccb21e938ba2d523c833de701
balint137 <dobszi14@gmail.com>
2018-04-07 14:09:25 +0200

    Teszt pálya tömb

--- src/common/GameState.java
+++ src/common/GameState.java
@@ -1,10 +1,30 @@
 package common;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 public class GameState implements Serializable {
 	private static final long serialVersionUID = 7413938669227547696L;
 
 	public enum FieldType { GROUND, WALL, PLAYER1, PLAYER2, CRATE, TARGET }
 	public FieldType[][] fields;
+	
+	public GameState() {
+		// palya rajzolas teszt
+		fields = new FieldType[10][10];
+		
+		for (FieldType[] row: fields) {
+			Arrays.fill(row, FieldType.GROUND);
+		}
+		
+		Arrays.fill(fields[1], FieldType.WALL);
+		Arrays.fill(fields[7], FieldType.WALL);
+		fields[2][3] = FieldType.CRATE;
+		fields[3][3] = FieldType.CRATE;
+		fields[5][7] = FieldType.CRATE;
+		fields[4][8] = FieldType.WALL;
+		fields[4][7] = FieldType.WALL;
+		fields[2][6] = FieldType.TARGET;
+		fields[5][5] = FieldType.PLAYER1;
+	}
 }
