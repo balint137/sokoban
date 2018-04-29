c2f1916e5a1731668e6e98318be793e6528a29b8
balint137 <dobszi14@gmail.com>
2018-04-29 21:54:13 +0200

    Koordináta osztály felesleges kód

--- src/common/Coordinate.java
+++ src/common/Coordinate.java
@@ -1,17 +1,20 @@
 package common;
 
-import java.util.Objects;
-
 import static gui.GUI.MAX_MAP_SIZE;
 
 public class Coordinate {
     private int x, y;
 
+    public Coordinate(int x, int y) {
+        setX(x);
+        setY(y);
+    }
+
     public int getX() {
         return x;
     }
 
-    public void setX(int x) {
+    public void setX(int x) throws IllegalArgumentException {
         if (x >= 0 && x < MAX_MAP_SIZE) {
             this.x = x;
         } else {
@@ -23,31 +26,11 @@ public class Coordinate {
         return y;
     }
 
-    public void setY(int y) {
+    public void setY(int y) throws IllegalArgumentException {
         if (y >= 0 && y < MAX_MAP_SIZE) {
             this.y = y;
         } else {
             throw new IllegalArgumentException("Invalid coordinate");
         }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Coordinate that = (Coordinate) o;
-        return getX() == that.getX() &&
-                getY() == that.getY();
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(getX(), getY());
-    }
-
-    public Coordinate(int x, int y) {
-        setX(x);
-        setY(y);
     }
 }
