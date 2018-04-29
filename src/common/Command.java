a52b7615f1e25a8ff4af2c67f331988e550f1bdb
balint137 <dobszi14@gmail.com>
2018-04-29 21:53:42 +0200

    Néhány command hozzáadva

--- src/common/Command.java
+++ src/common/Command.java
@@ -1,7 +1,19 @@
 package common;
 
+import java.awt.event.KeyEvent;
 import java.io.Serializable;
 
-public class Command implements Serializable{
-	private static final long serialVersionUID = -1189577598511558283L;
+public class Command implements Serializable {
+    public CommandType command;
+    public String mapFilePath;
+    public KeyEvent lastKeyPressed;
+
+    public Command(CommandType command, KeyEvent lastKeyPressed) {
+        this.command = command;
+        this.lastKeyPressed = lastKeyPressed;
+    }
+
+    public enum CommandType {
+        OPEN_MAP_FILE, NEW_GAME, KEY_PRESSED
+    }
 }
