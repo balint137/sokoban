4caca0b93bd063461b4ec13f8a9277f10be1487e
balint137 <dobszi14@gmail.com>
2018-04-30 14:27:42 +0200

    Gui: animáció osztály javítás, formázás, status bar

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -5,9 +5,8 @@ import common.GameState.FieldType;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
@@ -19,35 +18,47 @@ import java.util.Map;
 
 public class GUI extends JFrame implements IGameState, KeyListener {
     public static final int MAX_MAP_SIZE = 10;
-    private static final long serialVersionUID = 7803853539866953138L;
+
     private int imageSize;
     private Map<FieldType, BufferedImage> fieldToImage = new EnumMap<>(FieldType.class);
-    private FieldType[][] fields = new FieldType[MAX_MAP_SIZE][MAX_MAP_SIZE];
+
+    private FieldType[][] fields;
     private ArrayList<DynamicFieldAnimation> dynamicFieldAnimations;
+
     private ICommand logic;
 
-    GUI(ICommand logic) throws IOException {
+    public GUI() throws IOException {
         super("Sokoban");
-        this.logic = logic;
-        setSize(1000, 800);
+        setSize(850, 750);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
+        setLayout(new BorderLayout());
         setFocusable(true);
         addKeyListener(this);
+
+        fields = new FieldType[MAX_MAP_SIZE][MAX_MAP_SIZE];
+        dynamicFieldAnimations = new ArrayList<>();
 
         BuildMenu();
         ReadResourceImages();
 
         DrawPanel drawPanel = new DrawPanel();
-        getContentPane().add(BorderLayout.CENTER, drawPanel);
+        add(drawPanel, BorderLayout.CENTER);
+
+        JPanel statusPanel = new JPanel();
+        statusPanel.setBorder(new BevelBorder(BevelBorder.LOWERED));
+        add(statusPanel, BorderLayout.SOUTH);
+        statusPanel.setPreferredSize(new Dimension(getWidth(), 40));
+        statusPanel.setLayout(new BoxLayout(statusPanel, BoxLayout.X_AXIS));
+        JLabel statusLabel = new JLabel("Status bar");
+        statusLabel.setHorizontalAlignment(SwingConstants.LEFT);
+        statusPanel.add(statusLabel);
 
         setVisible(true);
     }
 
-    public static void main(String[] args) throws IOException {
-        GUI g = new GUI(null);
-        GameState gameState = new GameState();
-        g.onNewGameState(gameState);
+    public void setLogic(ICommand l) {
+        this.logic = l;
     }
 
     private void ReadResourceImages() throws IOException {
@@ -72,48 +83,31 @@ public class GUI extends JFrame implements IGameState, KeyListener {
         JMenu menu = new JMenu("Start");
 
         JMenuItem menuItem = new JMenuItem("Client");
-        menuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-            }
+        menuItem.addActionListener(e -> {
         });
         menu.add(menuItem);
 
         menuItem = new JMenuItem("Server");
-        menuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                final JFileChooser fileChooser = new JFileChooser();
-                fileChooser.showOpenDialog(null);
-            }
+        menuItem.addActionListener(e -> {
+            final JFileChooser fileChooser = new JFileChooser();
+            fileChooser.showOpenDialog(null);
         });
         menu.add(menuItem);
 
         menuItem = new JMenuItem("Local");
-        menuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-            }
+        menuItem.addActionListener(e -> {
         });
         menu.add(menuItem);
 
         menuItem = new JMenuItem("Local multiplayer");
-        menuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-            }
+        menuItem.addActionListener(e -> {
         });
         menu.add(menuItem);
 
         menuBar.add(menu);
 
         menuItem = new JMenuItem("Exit");
-        menuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-            }
-        });
+        menuItem.addActionListener(e -> System.exit(0));
         menuBar.add(menuItem);
 
         setJMenuBar(menuBar);
@@ -134,37 +128,44 @@ public class GUI extends JFrame implements IGameState, KeyListener {
 
     @Override
     public void onNewGameState(GameState g) {
-        fields = g.staticFields;
-        dynamicFieldAnimations = new ArrayList<>();
-
-        for (DynamicField f : g.dynamicFields) {
-            dynamicFieldAnimations.add(new DynamicFieldAnimation(f));
-        }
-
-        boolean inProgress = true;
-
-        while (inProgress) {
-            inProgress = false;
-            for (DynamicFieldAnimation f : dynamicFieldAnimations) {
-                if (f.dx != 0) {
-                    inProgress = true;
-                    f.dx = f.dx > 0 ? f.dx - 1 : f.dx + 1;
-                } else if (f.dy != 0) {
-                    inProgress = true;
-                    f.dy = f.dy > 0 ? f.dy - 1 : f.dy + 1;
+        switch (g.type) {
+            case STATIC_FIELDS:
+                fields = g.staticFields;
+                break;
+            case DYNAMIC_FIELDS:
+                dynamicFieldAnimations.clear();
+                for (DynamicField f : g.dynamicFields) {
+                    dynamicFieldAnimations.add(new DynamicFieldAnimation(f));
                 }
-                try {
-                    Thread.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+
+                boolean inProgress = true;
+                while (inProgress) {
+                    inProgress = false;
+                    for (DynamicFieldAnimation f : dynamicFieldAnimations) {
+                        if (f.dx != 0) {
+                            inProgress = true;
+                            f.dx = f.dx > 0 ? f.dx - 4 : f.dx + 4;
+                        } else if (f.dy != 0) {
+                            inProgress = true;
+                            f.dy = f.dy > 0 ? f.dy - 4 : f.dy + 4;
+                        }
+                        try {
+                            Thread.sleep(1);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        this.repaint();
+                    }
                 }
-                repaint();
-            }
+                break;
+            case TIME:
+            case MOVEMENTS:
+                break;
         }
+        Thread.currentThread().interrupt();
     }
 
     class DrawPanel extends JPanel {
-
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
@@ -181,20 +182,20 @@ public class GUI extends JFrame implements IGameState, KeyListener {
 
             //draw dynamic
             for (DynamicFieldAnimation f : dynamicFieldAnimations) {
-                g.drawImage(fieldToImage.get(f.field.type),
-                        (f.field.actual.getX() + f.field.delta.getX()) * imageSize - f.dx,
-                        (f.field.actual.getY() + f.field.delta.getY()) * imageSize - f.dy,
-                        null);
+                g.drawImage(fieldToImage.get(f.type), f.x - f.dx, f.y - f.dy, null);
             }
         }
     }
 
     class DynamicFieldAnimation {
-        DynamicField field;
+        GameState.FieldType type;
+        int x, y;
         int dx, dy;
 
         DynamicFieldAnimation(DynamicField field) {
-            this.field = field;
+            type = field.type;
+            x = (field.actual.getX() + field.delta.getX()) * imageSize;
+            y = (field.actual.getY() + field.delta.getY()) * imageSize;
             dx = field.delta.getX() * imageSize;
             dy = field.delta.getY() * imageSize;
         }
