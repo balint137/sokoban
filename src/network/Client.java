b7f0bdbc9070e2988ebe878fa5f42aad61cf5bda
fittori <38087203+fittori@users.noreply.github.com>
2018-06-01 13:02:26 +0200

    Update Client.java
    Javadoc comments

--- src/network/Client.java
+++ src/network/Client.java
@@ -10,13 +10,37 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
+
+/**
+ * The client class.
+ * ICommand is an interface to get information from the GUI. 
+ *
+ */
+
 public class Client implements ICommand {
+    
+     /**
+	 * IGameState is an interface that sends command information to the GUI.
+	 * We need a client socket for the network communication. 
+	 * The object in/outputstream reads/writes information between the socket and the client.
+	 */
+    
     private IGameState g;
     private Socket socket = null;
     private ObjectOutputStream out = null;
     private ObjectInputStream in = null;
 
-
+    /**
+     * The Client constructor.
+     * It creates a socket with a fixed 10007 port number and
+     * an ip address, that is read through the GUI.
+     * Then it creates the in/outpustream for the socket.
+     * It also creates a GUI instance g for the communication interface with the GUI.
+     * Finally it creates the receiver thread and starts it.
+     * @param gui
+     * @param ip
+     */
+    
     public Client(GUI gui, String ip) {
         g = gui;
 
@@ -33,6 +57,10 @@ public class Client implements ICommand {
         }
     }
 
+    /**
+     *  This function closes the in/outputstreams and the socket.
+     */
+    
     private void disconnect() {
         try {
             if (out != null)
@@ -46,12 +74,24 @@ public class Client implements ICommand {
             System.out.println(e.getMessage());
         }
     }
+    
+    /**
+     * When the logic calls onCommand with a GameState parameter, 
+     * this interface sends the new gamestate to the server.
+     * 
+     */
 
     @Override
     public void onCommand(Command c) {
         c.fromNetwork = true;
         send(c);
     }
+    
+    /**
+     * The function that the command is sent with.
+     * It writes the Command object c to the server's outputstream.
+     * @param c
+     */
 
     private void send(Command c) {
         if (out == null)
@@ -68,6 +108,12 @@ public class Client implements ICommand {
         }
     }
 
+     /**
+     * The ReceiverThread is reading the socket's inputstream and when it 
+     * receives the object it sends it forward to the GUI through the onNewGameState interface.
+     *
+     */
+    
     class ReceiverThread implements Runnable {
         public void run() {
             try {
